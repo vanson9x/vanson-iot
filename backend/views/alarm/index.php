9123d545d04cce524e4e506d271c73a5f316b487
Phạm Văn Sơn <vanson9x.it@gmail.com>
2017-04-11 18:42:28 +0700

    no message

--- backend/views/alarm/index.php
+++ backend/views/alarm/index.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="alarm-index">
 
-    <!-- <h1><?= Html::encode($this->title) ?></h1> -->
+    <h1><?= Html::encode($this->title) ?></h1> 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p class="clear">
